dc3c62a10b17484651d57c21cc7e67125b94fedb
Marek Reha <reha.marek@gmail.com>
2017-08-17 21:27:21 +0200

    Initial implementation of Document upload functionality
    FileSystemStorageService used for file manipulation on disk
    FileUploadController taking care about web requests handling

---
+++ src/main/java/osu/kip/prodmis/service/impl/StorageFileNotFoundException.java
@@ -0,0 +1,12 @@
+package osu.kip.prodmis.service.impl;
+
+public class StorageFileNotFoundException extends StorageException {
+
+    public StorageFileNotFoundException(String message) {
+        super(message);
+    }
+
+    public StorageFileNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
