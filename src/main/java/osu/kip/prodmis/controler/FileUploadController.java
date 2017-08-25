364f3f4ca62193cfd49a0553ca51475b53822a12
Marek Reha <reha.marek@gmail.com>
2017-08-25 23:41:08 +0200

    imports cleaning on FileUploadController.java

--- src/main/java/osu/kip/prodmis/controler/FileUploadController.java
+++ src/main/java/osu/kip/prodmis/controler/FileUploadController.java
@@ -3,26 +3,19 @@ package osu.kip.prodmis.controler;
 import java.io.IOException;
 import java.util.stream.Collectors;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.ServletRequestDataBinder;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.support.ByteArrayMultipartFileEditor;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -34,14 +27,7 @@ public class FileUploadController {
 
 	private final StorageService storageService;
 
-	
-//	@InitBinder
-//	protected void initBinder(HttpServletRequest request,
-//	        ServletRequestDataBinder binder) throws ServletException {
-//	    binder.registerCustomEditor(byte[].class,
-//	            new ByteArrayMultipartFileEditor());
-//	}
-	
+
 	
 	@Autowired
 	public FileUploadController(StorageService storageService) {
